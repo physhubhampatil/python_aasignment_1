bc6aa30313a13053dd682601d9151bfcc2e4fecc
ShubhamPatil <102431396+physhubhampatil@users.noreply.github.com>
2026-02-11 13:54:34 +0530

    Add files via upload

---
+++ mathop.py
@@ -0,0 +1,12 @@
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number; "))
+
+add = a + b
+sub = a - b
+mult = a * b
+div = a / b
+
+print("Addition : ",add)
+print("Subtraction : ",sub)
+print("Multiplication : ",mult)
+print("Division ; ",div)
